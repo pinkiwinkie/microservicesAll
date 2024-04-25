3f410de8d932c586adb42ed02c574e12db9fa75d
pinkiwinkie <elrumor99@gmail.com>
2024-04-25 09:18:43 +0200

    .

--- apigateway/src/main/java/com/helenaruiz/apigateway/config/SecurityConfig.java
+++ apigateway/src/main/java/com/helenaruiz/apigateway/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.helenaruiz.apigateway.config;
+/* package com.helenaruiz.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,3 +19,4 @@ public class SecurityConfig {
     return http.build();
   }
 }
+ */
